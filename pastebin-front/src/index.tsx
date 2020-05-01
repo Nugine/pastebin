fc1237c39a47c2b38e6329f48491b1f57cf9ba15
Nugine <nugine@foxmail.com>
2020-05-01 21:18:53 +0800

    front: remove cdn

--- pastebin-front/src/index.tsx
+++ pastebin-front/src/index.tsx
@@ -1,6 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "prismjs/themes/prism-coy.css";
+import "font-awesome/css/font-awesome.min.css";
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./app/App";
 
 ReactDOM.render(
