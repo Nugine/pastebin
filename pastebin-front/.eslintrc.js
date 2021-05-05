a80b2a6b3a58262951b161f4faf6052e07e1d30e
Nugine <nugine@foxmail.com>
2021-05-05 19:14:37 +0800

    front: minimal

---
+++ pastebin-front/.eslintrc.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+    },
+    plugins: [
+        "@typescript-eslint",
+    ],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+    ],
+    rules: {
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"],
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["warn"]
+    }
+};
