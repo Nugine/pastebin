5bb2dbaa147d1ecd8794cebf0bc856ff4ab6eae1
Nugine <nugine@foxmail.com>
2023-03-07 22:10:51 +0800

    front: init

---
+++ pastebin-front/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div>
+        <h1 id="announcement">越过长城，走向世界！</h1>
+    </div>
+</template>
+
+<style scoped>
+#announcement {
+    width: 100%;
+    text-align: center;
+}
+</style>
