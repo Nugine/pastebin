547a930fe3e264a44768b06c93d6d4329e0c4a8a
Nugine <nugine@foxmail.com>
2023-03-07 22:10:53 +0800

    front: editor

--- pastebin-front/src/pages/EditorPage.vue
+++ pastebin-front/src/pages/EditorPage.vue
@@ -1,30 +1,40 @@
 <template>
     <div class="btn-bar">
-        <button class="btn" type="button">Edit</button>
-        <button class="btn" type="button">Preview</button>
-        <button class="btn" type="button">Copy</button>
-        <button class="btn" type="button">Paste!</button>
+        <button class="btn" type="button" @click="handleEdit">编辑</button>
+        <button class="btn" type="button" @click="handlePreview">预览</button>
+        <button class="btn" type="button" @click="handleCopy">复制</button>
+        <button class="btn" type="button" @click="handlePaste">提交</button>
     </div>
     <form autocomplete="off">
         <div class="form-group">
-            <label>Title</label>
-            <input class="form-control" type="text" />
+            <label>标题</label>
+            <input class="form-control" type="text" v-model="store.record.title" />
         </div>
         <div class="form-group">
-            <label>Language</label>
-            <select class="form-control">
-                <option>test</option>
+            <label>语言</label>
+            <select class="form-control" v-model="store.record.lang">
+                <option v-for="lang in LANGS" :key="lang.value" :value="lang.value">
+                    {{ lang.display }}
+                </option>
             </select>
         </div>
         <div class="form-group">
-            <label>Expiration</label>
-            <select class="form-control">
-                <option>test</option>
+            <label>过期时间</label>
+            <select class="form-control" v-model="store.record.expiration_seconds">
+                <option v-for="exp in EXPIRATIONS" :key="exp.value" :value="exp.value">
+                    {{ exp.display }}
+                </option>
             </select>
         </div>
         <div class="form-group">
-            <label>Content</label>
-            <textarea class="form-control" rows="15" required ref="contentTextArea" />
+            <label>内容</label>
+            <textarea
+                class="form-control"
+                rows="15"
+                required
+                autofocus
+                v-model="store.record.content"
+            />
         </div>
     </form>
 </template>
@@ -37,8 +47,25 @@ form {
 </style>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { LANGS } from "@/data/lang";
+import { useStore } from "@/data/store";
+import { EXPIRATIONS } from "@/data/expiration";
 
-const contentTextArea = ref<HTMLTextAreaElement | null>(null);
-onMounted(() => contentTextArea.value?.focus());
+const store = useStore();
+
+function handleEdit() {
+    // TODO
+}
+
+function handlePreview() {
+    // TODO
+}
+
+function handleCopy() {
+    // TODO
+}
+
+function handlePaste() {
+    // TODO
+}
 </script>
