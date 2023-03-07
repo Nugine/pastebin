b2a5d7d5273b10c1d9011b79546ca73590dd56ee
Nugine <nugine@foxmail.com>
2023-03-07 22:10:52 +0800

    front: footer

--- pastebin-front/src/App.vue
+++ pastebin-front/src/App.vue
@@ -1,14 +1,16 @@
-<script setup lang="ts"></script>
-
 <template>
-    <div>
-        <h1 id="announcement">越过长城，走向世界！</h1>
+    <div style="flex-grow: 1">
+        <h1>越过长城，走向世界！</h1>
     </div>
+    <footer style="margin-bottom: 1em">
+        <span>
+            © 2019 - 2023
+            <a href="https://github.com/Nugine/pastebin" target="_blank">Nugine/pastebin</a>
+            AGPL-3.0
+        </span>
+    </footer>
 </template>
 
-<style scoped>
-#announcement {
-    width: 100%;
-    text-align: center;
-}
-</style>
+<style scoped></style>
+
+<script setup lang="ts"></script>
