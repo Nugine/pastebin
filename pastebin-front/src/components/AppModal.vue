bd1a0f8f9eb46da051df7bf1ee2b6fe2e1f101e2
Nugine <nugine@foxmail.com>
2023-03-07 22:10:55 +0800

    front: view: qrcode

---
+++ pastebin-front/src/components/AppModal.vue
@@ -0,0 +1,96 @@
+<template>
+    <Teleport to="body" v-if="mountTeleport">
+        <div class="fade backdrop" :class="{ show: show }"></div>
+        <div class="fade modal" :class="{ show: show }">
+            <div class="modal-dialog">
+                <slot/>
+            </div>
+        </div>
+    </Teleport>
+</template>
+
+<style scoped>
+.fade {
+    transition: opacity 0.15s linear;
+}
+
+.fade:not(.show) {
+    opacity: 0;
+}
+
+.backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1040;
+    width: 100vw;
+    height: 100vh;
+    background-color: #000;
+}
+
+.backdrop.show {
+    opacity: 0.5;
+}
+
+.modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1050;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    outline: 0;
+    display: block;
+}
+
+.modal-dialog {
+    position: relative;
+    width: auto;
+    max-width: 500px;
+    margin: 1.75rem auto;
+    pointer-events: auto;
+
+    display: flex;
+    flex-direction: column;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 0.3rem;
+    outline: 0;
+
+    transition: transform 0.2s ease-out;
+    transform: translateY(-50px);
+}
+
+.modal.show .modal-dialog {
+    transform: none;
+}
+</style>
+
+<script setup lang="ts">
+import { ref, watchPostEffect } from "vue";
+
+const props = defineProps<{ show: boolean }>();
+
+const mountTeleport = ref(props.show);
+const show = ref(false);
+
+watchPostEffect((onCleanup) => {
+    if (props.show) {
+        mountTeleport.value = true;
+        show.value = false;
+
+        // 挂载 Teleport 后，显示 Modal，触发 CSS 动画
+        const timer = setTimeout(() => (show.value = true), 60);
+        onCleanup(() => clearTimeout(timer));
+    } else {
+        // 触发 CSS 动画
+        show.value = false;
+
+        // 当动画结束后，卸载 Teleport
+        const timer = setTimeout(() => (mountTeleport.value = false), 400);
+        onCleanup(() => clearTimeout(timer));
+    }
+});
+</script>
--- pastebin-front/src/pages/ViewPage.vue
+++ pastebin-front/src/pages/ViewPage.vue
@@ -6,9 +6,20 @@
         <button class="btn" type="button" @click="handleCopy" :class="copyBtnClass">
             <IconCopy theme="outline" size="16" fill="#333" />
         </button>
-        <button class="btn" type="button" @click="handleQRCode">
+        <button class="btn" type="button" @click="qrcodeModal.open()">
             <IconQRCode theme="outline" size="16" fill="#333" :stroke-width="5" />
         </button>
+        <AppModal :show="qrcodeModal.show">
+            <div class="modal-header">
+                <span class="modal-title">{{ qrcodeModal.title }}</span>
+                <button class="btn" type="button" @click="qrcodeModal.close()">
+                    <IconClose theme="filled" size="16" fill="#333" />
+                </button>
+            </div>
+            <div class="modal-content">
+                <img :src="qrcodeModal.dataUrl" :alt="currentUrl" />
+            </div>
+        </AppModal>
         <button class="btn" type="button" @click="handleDownload">
             <IconDownload theme="outline" size="16" fill="#333" />
         </button>
@@ -19,16 +30,54 @@
     </div>
 </template>
 
+<style scoped>
+.modal-header {
+    width: 100%;
+    padding: 1rem;
+    border-bottom: 1px solid #dee2e6;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+}
+
+.modal-title {
+    font-size: 1.3rem;
+    font-weight: 400;
+}
+
+.modal-header .btn {
+    border: none;
+    box-shadow: none;
+}
+
+.modal-content {
+    width: 100%;
+    padding: 1rem;
+
+    display: flex;
+    justify-content: center;
+    flex: "1 1 auto";
+}
+</style>
+
 <script setup lang="ts">
 import IconEdit from "@icon-park/vue-next/es/icons/FileEditing";
 import IconCopy from "@icon-park/vue-next/es/icons/CopyOne";
 import IconQRCode from "@icon-park/vue-next/es/icons/PayCodeOne";
 import IconDownload from "@icon-park/vue-next/es/icons/Download";
 import IconView from "@icon-park/vue-next/es/icons/PreviewOpen";
+import IconClose from "@icon-park/vue-next/es/icons/Close";
+
+import { useRouter } from "vue-router";
+import { computed, onMounted, reactive } from "vue";
+
+import qrcode from "qrcode";
+
+import AppModal from "@/components/AppModal.vue";
 
 import { useCopyBtn } from "@/logic";
 import { useStore } from "@/data/store";
-import { useRouter } from "vue-router";
 
 const store = useStore();
 const router = useRouter();
@@ -45,9 +94,25 @@ const { copyBtnClass, handleCopy } = useCopyBtn(() => store.record.content);
 
 // 二维码 ----------------------------
 
-function handleQRCode() {
-    // TODO
-}
+const qrcodeModal = reactive({
+    show: false,
+
+    title: computed(() => store.record.title || "在线剪贴板"),
+
+    dataUrl: "",
+
+    open() {
+        this.show = true;
+    },
+    close() {
+        this.show = false;
+    },
+});
+
+const currentUrl = window.location.href;
+onMounted(() => {
+    qrcode.toDataURL(currentUrl, (_e, u) => (qrcodeModal.dataUrl = u));
+});
 
 // 下载 ----------------------------
 
