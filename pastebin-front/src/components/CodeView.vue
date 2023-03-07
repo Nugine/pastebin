7dbe5ddeb6b701551ae0e0af6affcd9639006bf5
Nugine <nugine@foxmail.com>
2023-03-07 22:10:57 +0800

    front: CodeVIew

---
+++ pastebin-front/src/components/CodeView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="line-numbers" ref="contentDiv">
+        <pre><code :class="prismClass">{{ props.content }}</code></pre>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref, watch } from "vue";
+import prismjs from "prismjs";
+
+const props = defineProps<{
+    lang: string;
+    content: string;
+}>();
+
+const contentDiv = ref<HTMLDivElement | null>(null);
+const prismClass = computed(() => `language-${props.lang}`);
+
+const highlight = () => {
+    const div = contentDiv.value;
+    if (div) {
+        prismjs.highlightAllUnder(div);
+    }
+};
+
+watch(
+    () => props.content,
+    (_newVal, _oldVal, onCleanup) => {
+        const timer = setTimeout(highlight, 60);
+        onCleanup(() => clearTimeout(timer));
+    },
+    { immediate: true, flush: "post" }
+);
+</script>
--- pastebin-front/src/components/XView.vue
+++ pastebin-front/src/components/XView.vue
@@ -3,20 +3,9 @@
         <div class="view-title" v-if="hasTitle">
             {{ props.record.title }}
         </div>
-        <div class="view-content line-numbers" ref="viewContentDiv">
-            <pre><code :class="prismClass">{{ props.record.content }}</code></pre>
-        </div>
+        <CodeView class="view-content" :lang="props.record.lang" :content="props.record.content" />
     </div>
 </template>
-
-<style>
-/* 修改 prismjs theme "coy" */
-pre[class*="language-"]::after,
-pre[class*="language-"]::before {
-    box-shadow: none !important;
-    display: none;
-}
-</style>
 
 <style scoped>
 .view-title {
@@ -35,31 +24,12 @@ pre[class*="language-"]::before {
 
 <script setup lang="ts">
 import type { PastebinRecord } from "@/data/dto";
-import { computed, ref, watch } from "vue";
-import prismjs from "prismjs";
+import { computed } from "vue";
+import CodeView from "./CodeView.vue";
 
 const props = defineProps<{
     record: PastebinRecord;
 }>();
 
 const hasTitle = computed(() => props.record.title !== "");
-
-const prismClass = computed(() => `language-${props.record.lang}`);
-
-const viewContentDiv = ref<HTMLDivElement | null>(null);
-const highlight = () => {
-    const div = viewContentDiv.value;
-    if (div) {
-        prismjs.highlightAllUnder(div);
-    }
-};
-
-watch(
-    () => props.record.content,
-    (_newVal, _oldVal, onCleanup) => {
-        const timer = setTimeout(highlight, 60);
-        onCleanup(() => clearTimeout(timer));
-    },
-    { immediate: true, flush: "post" }
-);
 </script>
